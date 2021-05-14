caae7c0b574eb8886971fff8852e1d565b880c42
Hyoki Min <raniless@naver.com>
2021-05-14 23:56:03 +0900

    상속관계 매핑

---
+++ src/main/java/extendsmapping/ExtendsRunMain.java
@@ -0,0 +1,42 @@
+package extendsmapping;
+
+import hellojpa.Member;
+import hellojpa.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class ExtendsRunMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Movie movie = new Movie();
+            movie.setDirector("AA");
+            movie.setActor("BB");
+            movie.setName("테스트무비");
+            movie.setPrice(1000);
+
+            em.persist(movie);
+
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie :" + findMovie);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
