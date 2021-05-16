daf7478bf574eb160d742de9ed991d503f1d7596
Hyoki Min <raniless@naver.com>
2021-05-16 16:10:44 +0900

    프록시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import org.hibernate.Hibernate;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -13,26 +15,41 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 저장
-            Team team = new Team();
-            team.setName("TeamA");
-            em.persist(team);
-
-            Member member = new Member();
-            member.setUserName("member1");
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUserName("member1");
+            em.persist(member1);
 
             em.flush();
             em.clear();
 
-            Member findMember = em.find(Member.class, member.getId());
-            //양방향 연관관계
-            for(Member m : findMember.getTeam().getMembers()) {
-                System.out.println("m = " + m.getUserName());
-            }
+            /*
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getClass());
 
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass());
+
+            System.out.println("refMember == findMember: " + (refMember == findMember));
+            */
+
+            /*
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass());
+
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getClass());
+
+            System.out.println("refMember == findMember: " + (refMember == findMember));
+            */
+
+            Member refMember = em.getReference(Member.class, member1.getId());
+            System.out.println("refMember isLoaded : " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+
+//            refMember.getUserName();
+            Hibernate.initialize(refMember);    //강제 초기화
+
+            System.out.println("refMember isLoaded : " + emf.getPersistenceUnitUtil().isLoaded(refMember));
+            System.out.println("Proxy Class Name : " + refMember.getClass().getName());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
