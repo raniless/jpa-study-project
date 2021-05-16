1cb8c2ebbabdcf02c261e4ca6e4b38f528c94d5f
Hyoki Min <raniless@naver.com>
2021-05-16 22:28:50 +0900

    CASCADE와 고아객체

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,11 +5,10 @@ import org.hibernate.Hibernate;
 import javax.persistence.*;
 
 public class JpaMain {
-    public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        PersistenceUnitUtil puu = emf.getPersistenceUnitUtil();
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    private static EntityManager em = emf.createEntityManager();
 
+    public static void main(String[] args) {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
@@ -19,7 +18,9 @@ public class JpaMain {
             //8-1 프록시
             //proxyMethod(puu, em);
             //8-2 즉시 로딩과 지연 로딩
-            lazyAndEagerLoadingMethod(em);
+            //lazyAndEagerLoadingMethod();
+            //8-3 영속성전이(CASCADE)
+            cascadeAndOrphanMethod();
 
             tx.commit();
         } catch (Exception e) {
@@ -31,7 +32,8 @@ public class JpaMain {
         emf.close();
     }
 
-    private static void proxyMethod(PersistenceUnitUtil puu, EntityManager em) {
+
+    private static void proxyMethod() {
         Member member1 = new Member();
         member1.setUserName("member1");
         em.persist(member1);
@@ -39,37 +41,38 @@ public class JpaMain {
         em.flush();
         em.clear();
 
-            /*
-            Member findMember = em.find(Member.class, member1.getId());
-            System.out.println("findMember = " + findMember.getClass());
-
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass());
-
-            System.out.println("refMember == findMember: " + (refMember == findMember));
-            */
-
-            /*
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass());
-
-            Member findMember = em.find(Member.class, member1.getId());
-            System.out.println("findMember = " + findMember.getClass());
-
-            System.out.println("refMember == findMember: " + (refMember == findMember));
-            */
+        /*
+        Member findMember = em.find(Member.class, member1.getId());
+        System.out.println("findMember = " + findMember.getClass());
 
         Member refMember = em.getReference(Member.class, member1.getId());
-        System.out.println("refMember isLoaded : " + puu.isLoaded(refMember));
+        System.out.println("refMember = " + refMember.getClass());
 
-//            refMember.getUserName();
+        System.out.println("refMember == findMember: " + (refMember == findMember));
+        */
+
+        /*
+        Member refMember = em.getReference(Member.class, member1.getId());
+        System.out.println("refMember = " + refMember.getClass());
+
+        Member findMember = em.find(Member.class, member1.getId());
+        System.out.println("findMember = " + findMember.getClass());
+
+        System.out.println("refMember == findMember: " + (refMember == findMember));
+        */
+
+        PersistenceUnitUtil persistenceUnitUtil = emf.getPersistenceUnitUtil();
+        Member refMember = em.getReference(Member.class, member1.getId());
+        System.out.println("refMember isLoaded : " + persistenceUnitUtil.isLoaded(refMember));
+
+//        refMember.getUserName();
         Hibernate.initialize(refMember);    //강제 초기화
 
-        System.out.println("refMember isLoaded : " + puu.isLoaded(refMember));
+        System.out.println("refMember isLoaded : " + persistenceUnitUtil.isLoaded(refMember));
         System.out.println("Proxy Class Name : " + refMember.getClass().getName());
     }
 
-    private static void lazyAndEagerLoadingMethod(EntityManager em) {
+    private static void lazyAndEagerLoadingMethod() {
         Team team = new Team();
         team.setName("teamA");
         em.persist(team);
@@ -91,5 +94,25 @@ public class JpaMain {
         //실제 사용하는 시점에 초기화(DB 조회)
         m.getTeam().getName();
         System.out.println("============================");
+    }
+
+    private static void cascadeAndOrphanMethod() {
+        Child child1 = new Child();
+        child1.setName("child1");
+        Child child2 = new Child();
+        child2.setName("child2");
+
+        Parent parent = new Parent();
+        parent.addChild(child1);
+        parent.addChild(child2);
+
+        em.persist(parent);
+
+        em.flush();
+        em.clear();
+
+        //고아객체
+        Parent findParent = em.find(Parent.class, parent.getId());
+        findParent.getChildList().remove(0);    //부모엔티티와 연관관계가 끊어짐 -> DELETE문 수행
     }
 }
