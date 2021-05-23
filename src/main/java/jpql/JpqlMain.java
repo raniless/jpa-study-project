f24bfe21a814c1709a92bcc8c01383129d21b51d
Hyoki Min <raniless@naver.com>
2021-05-24 00:00:19 +0900

    패치 조인

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -32,7 +32,14 @@ public class JpqlMain {
             //conditionalMethod();
 
             //JPQL 함수
-            jpqlFunctionMethod();
+            //jpqlFunctionMethod();
+
+            //경로표현식
+            //pathExprMethod();
+
+            //패치 조인(fetch join)
+            fetchJoinMethod();
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -230,6 +237,55 @@ public class JpqlMain {
                                     .getResultList();
         for (Integer integer : sizeResult) {
             System.out.println("integer = " + integer);
+        }
+    }
+
+    private static void pathExprMethod() {
+
+    }
+
+    private static void fetchJoinMethod() {
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
+        em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
+
+        em.flush();
+        em.clear();
+
+//        String query = "select m from Member m";
+//        String query = "select m from Member m join fetch m.team";
+//        List<Member> result = em.createQuery(query, Member.class)
+//                                .getResultList();
+//
+//        for (Member member : result) {
+//            System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//        }
+
+        String query = "select distinct t from Team t join fetch t.members";
+        List<Team> result = em.createQuery(query, Team.class)
+                                .getResultList();
+
+        for (Team team : result) {
+            System.out.println("team = " + team.getName()+ ", members= " + team.getMembers().size());
         }
     }
 }
