d190a8ea8c47ba3e040b6e4b61f16bb02c86fbc7
Hyoki Min <raniless@naver.com>
2021-06-05 08:07:02 +0900

    엔티티 직접 사용

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -38,8 +38,10 @@ public class JpqlMain {
             //pathExprMethod();
 
             //패치 조인(fetch join)
-            fetchJoinMethod();
+            //fetchJoinMethod();
 
+            //엔티티 직접 사용
+            entityUseMethod();
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -287,5 +289,40 @@ public class JpqlMain {
         for (Team team : result) {
             System.out.println("team = " + team.getName()+ ", members= " + team.getMembers().size());
         }
+    }
+
+    private static void entityUseMethod() {
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
+        em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
+
+        em.flush();
+        em.clear();
+
+        String query = "select m from Member m where m = :member";
+        Member findMember = em.createQuery(query, Member.class)
+                .setParameter("member", member1)
+                .getSingleResult();
+
+        System.out.println("findMember = " + findMember);
     }
 }
