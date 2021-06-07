43ac20add533c24c601804e068b6d7d58dc8839d
Hyoki Min <raniless@naver.com>
2021-06-07 22:01:02 +0900

    벌크 연산

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -44,7 +44,10 @@ public class JpqlMain {
             //entityUseMethod();
 
             //Named 쿼리
-            namedQueryMethod();
+            //namedQueryMethod();
+
+            //벌크연산
+            bulkOperationMethod();
 
             tx.commit();
         } catch (Exception e) {
@@ -364,5 +367,38 @@ public class JpqlMain {
         for (Member member : resultList) {
             System.out.println("member = " + member);
         }
+    }
+
+    private static void bulkOperationMethod() {
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
+        em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
+
+        em.flush();
+        em.clear();
+
+        int resultCount = em.createQuery("update Member m set m.age = 20")
+                .executeUpdate();
+
+        System.out.println("resultCount = " + resultCount);
     }
 }
