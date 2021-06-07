154abdf547f60008bb5c1741af1eca07962d3a96
Hyoki Min <raniless@naver.com>
2021-06-07 21:46:42 +0900

    Named 쿼리

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -41,7 +41,11 @@ public class JpqlMain {
             //fetchJoinMethod();
 
             //엔티티 직접 사용
-            entityUseMethod();
+            //entityUseMethod();
+
+            //Named 쿼리
+            namedQueryMethod();
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
@@ -324,5 +328,41 @@ public class JpqlMain {
                 .getSingleResult();
 
         System.out.println("findMember = " + findMember);
+    }
+
+    private static void namedQueryMethod() {
+        Team teamA = new Team();
+        teamA.setName("팀A");
+        em.persist(teamA);
+
+        Team teamB = new Team();
+        teamB.setName("팀B");
+        em.persist(teamB);
+
+        Member member1 = new Member();
+        member1.setUsername("회원1");
+        member1.setTeam(teamA);
+        em.persist(member1);
+
+        Member member2 = new Member();
+        member2.setUsername("회원2");
+        member2.setTeam(teamA);
+        em.persist(member2);
+
+        Member member3 = new Member();
+        member3.setUsername("회원3");
+        member3.setTeam(teamB);
+        em.persist(member3);
+
+        em.flush();
+        em.clear();
+
+        List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
+                .setParameter("username", "회원1")
+                .getResultList();
+
+        for (Member member : resultList) {
+            System.out.println("member = " + member);
+        }
     }
 }
